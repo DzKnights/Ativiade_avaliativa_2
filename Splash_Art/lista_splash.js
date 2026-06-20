62ecd5910d4c4021569819c4aa9fc651489001fd
W.G.P <103127318+DzKnights@users.noreply.github.com>
2026-06-20 20:58:48 -0300

    Add files via upload

---
+++ Splash_Art/lista_splash.js
@@ -0,0 +1,115 @@
+const LISTA_SPLASH = [
+
+    // AATROX
+    { campeao: "Aatrox", skin: "Mecha Aatrox", imagem: "splashArt/Aatrox_1.jpg" },
+    { campeao: "Aatrox", skin: "Aatrox Lua Sangrenta", imagem: "splashArt/Aatrox_2.jpg" },
+    { campeao: "Aatrox", skin: "Aatrox Primordiano", imagem: "splashArt/Aatrox_3.jpg" },
+    { campeao: "Aatrox", skin: "Aatrox DRX de Prestígio", imagem: "splashArt/Aatrox_4.jpg" },
+    { campeao: "Aatrox", skin: "Aatrox DRX", imagem: "splashArt/Aatrox_5.jpg" },
+
+    // ALISTAR
+    { campeao: "Alistar", skin: "Alistar Negro", imagem: "splashArt/alistar_1.jpg" },
+    { campeao: "Alistar", skin: "Alistar Conquistador", imagem: "splashArt/alistar_2.jpg" },
+    { campeao: "Alistar", skin: "Alistar Sabugueiro", imagem: "splashArt/alistar_3.jpg" },
+    { campeao: "Alistar", skin: "Alistar Velho Oeste", imagem: "splashArt/alistar_4.jpg" },
+    { campeao: "Alistar", skin: "Alistar Vaquinha", imagem: "splashArt/alistar_5.jpg" },
+
+    // DARIUS
+    { campeao: "Darius", skin: "Darius Reinos Mech", imagem: "splashArt/Darius_1.jpg" },
+    { campeao: "Darius", skin: "Deus-Rei Darius Divino", imagem: "splashArt/Darius_2.jpg" },
+    { campeao: "Darius", skin: "Darius Velho Oeste", imagem: "splashArt/Darius_3.jpg" },
+    { campeao: "Darius", skin: "Darius Mestre da Enterrada", imagem: "splashArt/Darius_4.jpg" },
+    { campeao: "Darius", skin: "Deus-Rei Darius", imagem: "splashArt/Darius_5.jpg" },
+
+    // GAREN
+    { campeao: "Garen", skin: "Garen Errante", imagem: "splashArt/garen_1.jpg" },
+    { campeao: "Garen", skin: "Deus-Rei Garen Caído", imagem: "splashArt/garen_2.jpg" },
+    { campeao: "Garen", skin: "Garen Legionário de Aço", imagem: "splashArt/garen_3.jpg" },
+    { campeao: "Garen", skin: "Garen Reinos Mech", imagem: "splashArt/garen_4.jpg" },
+    { campeao: "Garen", skin: "Garen Demacia Vice", imagem: "splashArt/garen_5.jpg" },
+
+    // IRELIA
+    { campeao: "Irelia", skin: "Irelia Lâminas Gélidas", imagem: "splashArt/irelia_1.jpg" },
+    { campeao: "Irelia", skin: "Irelia Florescer Espiritual", imagem: "splashArt/irelia_2.jpg" },
+    { campeao: "Irelia", skin: "Irelia Velho Oeste", imagem: "splashArt/irelia_3.jpg" },
+    { campeao: "Irelia", skin: "Projeto: Irelia", imagem: "splashArt/irelia_4.jpg" },
+    { campeao: "Irelia", skin: "Irelia Pão Quentinho", imagem: "splashArt/irelia_5.jpg" },
+
+    // KAI'SA
+    { campeao: "Kai'Sa", skin: "Kai'Sa Tinta Sombria", imagem: "splashArt/kaisa_1.jpg" },
+    { campeao: "Kai'Sa", skin: "Kai'Sa Lenda Ascendida", imagem: "splashArt/kaisa_2.jpg" },
+    { campeao: "Kai'Sa", skin: "Kai'Sa Disparos Angelicais", imagem: "splashArt/kaisa_3.jpg" },
+    { campeao: "Kai'Sa", skin: "Kai'Sa Guardiã Estelar", imagem: "splashArt/kaisa_4.jpg" },
+    { campeao: "Kai'Sa", skin: "Kai'Sa Fliperama", imagem: "splashArt/kaisa_5.jpg" },
+
+    // KATARINA
+    { campeao: "Katarina", skin: "Escolhida do Lobo", imagem: "splashArt/katarina_1.jpg" },
+    { campeao: "Katarina", skin: "Rainha de Batalha", imagem: "splashArt/katarina_2.jpg" },
+    { campeao: "Katarina", skin: "Pétalas da Primavera", imagem: "splashArt/katarina_3.jpg" },
+    { campeao: "Katarina", skin: "Velho Oeste", imagem: "splashArt/katarina_4.jpg" },
+    { campeao: "Katarina", skin: "Lua Sangrenta", imagem: "splashArt/katarina_5.jpg" },
+
+    // LISSANDRA
+    { campeao: "Lissandra", skin: "Porcelana de Prestígio", imagem: "splashArt/lissandra_1.jpg" },
+    { campeao: "Lissandra", skin: "Donzela de Ferro", imagem: "splashArt/lissandra_2.jpg" },
+    { campeao: "Lissandra", skin: "Congregação das Bruxas", imagem: "splashArt/lissandra_3.jpg" },
+    { campeao: "Lissandra", skin: "Cosmo Negro", imagem: "splashArt/lissandra_4.jpg" },
+    { campeao: "Lissandra", skin: "Embalos no Espaço", imagem: "splashArt/lissandra_5.jpg" },
+
+    // MASTER YI
+    { campeao: "Master Yi", skin: "Assassino", imagem: "splashArt/MasterYi_1.webp" },
+    { campeao: "Master Yi", skin: "O Escolhido", imagem: "splashArt/MasterYi_2.webp" },
+    { campeao: "Master Yi", skin: "Projeto Yi Linha de Comando", imagem: "splashArt/MasterYi_3.jpg" },
+    { campeao: "Master Yi", skin: "Boneco de Neve", imagem: "splashArt/MasterYi_4.jpg" },
+    { campeao: "Master Yi", skin: "Espada Cósmica", imagem: "splashArt/MasterYi_5.jpg" },
+
+    // MORDEKAISER
+    { campeao: "Mordekaiser", skin: "Sahn-Uzal", imagem: "splashArt/mordekaiser_1.webp" },
+    { campeao: "Mordekaiser", skin: "Dracônico", imagem: "splashArt/mordekaiser_2.jpg" },
+    { campeao: "Mordekaiser", skin: "Estrela Negra", imagem: "splashArt/mordekaiser_3.jpg" },
+    { campeao: "Mordekaiser", skin: "Deus Antigo", imagem: "splashArt/mordekaiser_4.jpg" },
+    { campeao: "Mordekaiser", skin: "Velho Oeste", imagem: "splashArt/mordekaiser_5.jpg" },
+
+    // RIVEN
+    { campeao: "Riven", skin: "Pacto Quebrado", imagem: "splashArt/riven_1.jpg" },
+    { campeao: "Riven", skin: "Florescer Espiritual", imagem: "splashArt/riven_2.jpg" },
+    { campeao: "Riven", skin: "Emboscada Primordial", imagem: "splashArt/riven_3.jpg" },
+    { campeao: "Riven", skin: "Coelha de Batalha", imagem: "splashArt/riven_4.jpg" },
+    { campeao: "Riven", skin: "Emissária da Luz", imagem: "splashArt/riven_5.jpg" },
+
+    // TRYNDAMERE
+    { campeao: "Tryndamere", skin: "Terror Noturno", imagem: "splashArt/tryndamere_1.jpg" },
+    { campeao: "Tryndamere", skin: "Lua Sangrenta", imagem: "splashArt/tryndamere_2.jpg" },
+    { campeao: "Tryndamere", skin: "Sultão", imagem: "splashArt/tryndamere_3.jpg" },
+    { campeao: "Tryndamere", skin: "Quimtec", imagem: "splashArt/tryndamere_4.jpg" },
+    { campeao: "Tryndamere", skin: "Visões dos Caídos", imagem: "splashArt/tryndamere_5.jpg" },
+
+    // VAYNE
+    { campeao: "Vayne", skin: "Projeto Vayne", imagem: "splashArt/vayne_1.jpg" },
+    { campeao: "Vayne", skin: "Sentinela", imagem: "splashArt/vayne_2.jpg" },
+    { campeao: "Vayne", skin: "Emissária da Luz", imagem: "splashArt/vayne_3.jpg" },
+    { campeao: "Vayne", skin: "Fogos de Artifício", imagem: "splashArt/vayne_4.jpg" },
+    { campeao: "Vayne", skin: "Ladra de Almas", imagem: "splashArt/vayne_5.jpg" },
+
+    // VOLIBEAR
+    { campeao: "Volibear", skin: "Capitão Volibear", imagem: "splashArt/volibear_1.jpg" },
+    { campeao: "Volibear", skin: "O Urso dos Mil Flagelos", imagem: "splashArt/volibear_2.jpg" },
+    { campeao: "Volibear", skin: "Tinta Sombria", imagem: "splashArt/volibear_3.jpg" },
+    { campeao: "Volibear", skin: "Armadura Rúnica", imagem: "splashArt/volibear_4.jpg" },
+    { campeao: "Volibear", skin: "Nevasca do Ártico", imagem: "splashArt/volibear_5.jpg" },
+
+    // YASUO
+    { campeao: "Yasuo", skin: "Tinta Sombria de Prestígio", imagem: "splashArt/yasuo_1.jpg" },
+    { campeao: "Yasuo", skin: "Emissário da Escuridão", imagem: "splashArt/yasuo_2.jpg" },
+    { campeao: "Yasuo", skin: "Lua Sangrenta", imagem: "splashArt/yasuo_3.jpg" },
+    { campeao: "Yasuo", skin: "Profetizado", imagem: "splashArt/yasuo_4.jpg" },
+    { campeao: "Yasuo", skin: "Dragão da Verdade", imagem: "splashArt/yasuo_5.jpg" },
+
+    // ZED
+    { campeao: "Zed", skin: "Projeto Zed", imagem: "splashArt/zed_1.jpg" },
+    { campeao: "Zed", skin: "Zed Mundial 2016", imagem: "splashArt/zed_2.jpg" },
+    { campeao: "Zed", skin: "Zed Galante", imagem: "splashArt/zed_3.jpg" },
+    { campeao: "Zed", skin: "Zed Jurado de Morte", imagem: "splashArt/zed_4.jpg" },
+    { campeao: "Zed", skin: "Zed Lua Sangrenta", imagem: "splashArt/zed_5.jpg" }
+
+];
---
+++ Splash_Art/splash_campeos.js
@@ -0,0 +1,101 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const splash = document.getElementById("splash-img");
+    const input = document.getElementById("input-campeao");
+    const btn = document.getElementById("btn-enviar");
+    const contador = document.getElementById("contador-tentativas");
+
+    // Verifica se a lista carregou
+    if (!LISTA_SPLASH || LISTA_SPLASH.length === 0) {
+        alert("Erro: LISTA_SPLASH não foi carregada.");
+        return;
+    }
+
+    // Escolhe uma splash aleatória
+    const splashAtual =
+        LISTA_SPLASH[Math.floor(Math.random() * LISTA_SPLASH.length)];
+
+    console.log("Splash escolhida:", splashAtual);
+
+    // Carrega imagem
+    splash.src = splashAtual.imagem;
+
+    // Zoom inicial
+    splash.className = "zoom-1";
+
+    // Ponto aleatório da splash
+    const posX = Math.floor(Math.random() * 100);
+    const posY = Math.floor(Math.random() * 100);
+
+    splash.style.objectPosition = `${posX}% ${posY}%`;
+
+    let tentativas = 0;
+
+    function verificarResposta() {
+
+        const resposta =
+            input.value.trim().toLowerCase();
+
+        if (resposta === "") return;
+
+        tentativas++;
+
+        contador.textContent =
+            `Tentativas: ${tentativas}`;
+
+        if (
+            resposta ===
+            splashAtual.campeao.toLowerCase()
+        ) {
+
+            splash.className = "revelado";
+
+            setTimeout(() => {
+                alert(
+                    `Parabéns!\n\nCampeão: ${splashAtual.campeao}\nSkin: ${splashAtual.skin}`
+                );
+            }, 300);
+
+            return;
+        }
+
+        switch (tentativas) {
+
+            case 1:
+                splash.className = "zoom-2";
+                break;
+
+            case 2:
+                splash.className = "zoom-3";
+                break;
+
+            case 3:
+                splash.className = "zoom-4";
+                break;
+
+            default:
+
+                splash.className = "revelado";
+
+                setTimeout(() => {
+                    alert(
+                        `Fim de jogo!\n\nEra ${splashAtual.campeao}\nSkin: ${splashAtual.skin}`
+                    );
+                }, 300);
+
+                btn.disabled = true;
+                input.disabled = true;
+        }
+
+        input.value = "";
+    }
+
+    btn.addEventListener("click", verificarResposta);
+
+    input.addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
+            verificarResposta();
+        }
+    });
+
+});

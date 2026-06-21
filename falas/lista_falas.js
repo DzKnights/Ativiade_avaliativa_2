3d232e4ad522863ca1f80492e63938dac95d75b9
Favoretto04 <kauefavoretto2004@gmail.com>
2026-06-20 23:44:53 -0300

    Update lista_falas.js

--- falas/lista_falas.js
+++ falas/lista_falas.js
@@ -3,120 +3,120 @@ const LISTA_CAMPEOES = [
         nome: "Aatrox",
         frase: "Eu não sou o seu inimigo, eu sou O Inimigo!",
         audio: "sons/aatrox.mp3",
-        imagem: "../default/icone_champions/aatrox.jpg"
+        imagem: "../icone_champions/aatrox.jpg"
     },
     {
         nome: "Ahri",
         frase: "Agora eles são meus!",
         audio: "sons/ahri.mp3",
-        imagem: "../default/icone_champions/ahri.jpg"
+        imagem: "../icone_champions/ahri.jpg"
     },
     {
         nome: "Lulu",
         frase: "AhhahahAHHAahahAHHahahAHHa",
         audio: "sons/lulu.mp3",
-        imagem: "../default/icone_champions/lulu.jpg"
+        imagem: "../icone_champions/lulu.jpg"
     },
     {
         nome: "Shaco",
         frase: "Que tal um truque de Mágica?",
         audio: "sons/shaco.mp3",
-        imagem: "../default/icone_champions/shaco.jpg"
+        imagem: "../icone_champions/shaco.jpg"
     },
     {
         nome: "Lee Sin",
         frase: "Sua vontade, minha mão.",
         audio: "sons/leesin.mp3",
-        imagem: "../default/icone_champions/leesin.jpg"
+        imagem: "../icone_champions/leesin.jpg"
     },
     {
         nome: "Kog Maw",
         frase: "tan tan tan tan tan tan tan tan",
         audio: "sons/kogmaw.mp3",
-        imagem: "../default/icone_champions/kogmaw.jpg"
+        imagem: "../icone_champions/kogmaw.jpg"
     },
     {
         nome: "Fiddlesticks",
         frase: "Estou sempre aqui!",
         audio: "sons/fiddlesticks.mp3",
-        imagem: "../default/icone_champions/fiddlesticks.jpg"
+        imagem: "../icone_champions/fiddlesticks.jpg"
     },
     {
         nome: "Gangplank",
         frase: "As profundezas chamam!",
         audio: "sons/gangplank.mp3",
-        imagem: "../default/icone_champions/gangplank.jpg"
+        imagem: "../icone_champions/gangplank.jpg"
     },
     {
         nome: "Neeko",
         frase: "Que legal tá tudo desabando",
         audio: "sons/neeko.mp3",
-        imagem: "../default/icone_champions/neeko.jpg"
+        imagem: "../icone_champions/neeko.jpg"
     },
     {
         nome: "Twitch",
         frase: "É meu, eu que lambi!",
         audio: "sons/twitch.mp3",
-        imagem: "../default/icone_champions/twitch.jpg"
+        imagem: "../icone_champions/twitch.jpg"
     },
     {
         nome: "Darius",
         frase: "Somente eu, posso nos levar a vitória!",
         audio: "sons/darius.mp3",
-        imagem: "../default/icone_champions/darius.jpg"
+        imagem: "../icone_champions/darius.jpg"
     },
     {
         nome: "Kennen",
         frase: "Uma morte siliciosa!",
         audio: "sons/kennen.mp3",
-        imagem: "../default/icone_champions/kennen.jpg"
+        imagem: "../icone_champions/kennen.jpg"
     },
     {
         nome: "Mordekaiser",
         frase: "Todos servirão o senhor, dos vivos e dos mortos!",
         audio: "sons/mordekaiser.mp3",
-        imagem: "../default/icone_champions/mordekaiser.jpg"
+        imagem: "../icone_champions/mordekaiser.jpg"
     },
     {
         nome: "Dr Mundo",
         frase: "Pra ser rico, tem que não ser pobre!",
         audio: "sons/drmundo.mp3",
-        imagem: "../default/icone_champions/drmundo.jpg"
+        imagem: "../icone_champions/drmundo.jpg"
     },
     {
         nome: "Nidalee",
         frase: "Sabia que estamos em época de acasalamento? Hahahaha!",
         audio: "sons/nidalee.mp3",
-        imagem: "../default/icone_champions/nidalee.jpg"
+        imagem: "../icone_champions/nidalee.jpg"
     },
     {
         nome: "Master Yi",
         frase: "O segredo para a imortalidade? Não morra!",
         audio: "sons/master_yi.mp3",
-        imagem: "../default/icone_champions/master_yi.jpg"
+        imagem: "../icone_champions/master_yi.jpg"
     },
     {
         nome: "Rammus",
         frase: "Ok!",
         audio: "sons/rammus.mp3",
-        imagem: "../default/icone_champions/rammus.jpg"
+        imagem: "../icone_champions/rammus.jpg"
     },
     {
         nome: "Shen",
         frase: "Você já esta morto, só não percebeu ainda!",
         audio: "sons/shen.mp3",
-        imagem: "../default/icone_champions/shen.jpg"
+        imagem: "../icone_champions/shen.jpg"
     },
     {
         nome: "Kled",
         frase: "Meu sangue é Mijo! E vinagre! Oh coisa boa!",
         audio: "sons/kled.mp3",
-        imagem: "../default/icone_champions/kled.jpg"
+        imagem: "../icone_champions/kled.jpg"
     },
     {
         nome: "Tahm Kench",
         frase: "Você é tão elegante, quanto um banheiro de hospício!",
         audio: "sons/tahmkench.mp3",
-        imagem: "../default/icone_champions/tahmkench.jpg"
+        imagem: "../icone_champions/tahmkench.jpg"
     },
 ];
